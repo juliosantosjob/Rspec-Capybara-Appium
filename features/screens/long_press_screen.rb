22d10c4d485c3d4d7a8c23d1658ea1d5c901f3a1
Julio Santos <julioc.9730@gmail.com>
2024-02-12 10:19:27 -0300

    Update long_press_screen.rb

--- features/screens/long_press_screen.rb
+++ features/screens/long_press_screen.rb
@@ -1,1 +1,15 @@
+require_relative "../support/capy"
+require_relative "../support/base_page"
+require_relative "../utils/helpers"
 
+class LongPressScreen < BasePage
+  include Helper
+
+  element :btn_long_press, "xpath://android.widget.TextView[@content-desc='longPress']", "locator:ios"
+  element :fld_title_long_press, "xpath://android.widget.TextView[@text='Long Press Demo']", "locator:ios"l
+
+  def access_double_tap_screen
+    wait_and_tap(btn_long_press, 5)
+    assert_visible(fld_title_long_press)
+  end
+end
