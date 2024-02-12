70e642c015c73a0a18c9815833a7ef3300e1811b
Julio Santos <julioc.9730@gmail.com>
2024-02-12 10:25:24 -0300

    Update long_press_spec.rb

--- features/specs/long_press_spec.rb
+++ features/specs/long_press_spec.rb
@@ -1,1 +1,13 @@
+require_relative "../screens/long_press_screen"
 
+RSpec.describe "Long press button", :regression do
+  before do
+    @login.go_app
+    @login.fill("admin", "admin")
+    @login.at_home
+  end
+
+  it "Checking long press", :long_press do
+    @long_press.access_long_press_screen
+  end
+end
