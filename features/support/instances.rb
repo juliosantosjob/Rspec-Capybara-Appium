661a801c32150704b2936fde89c5bb56047dffde
Julio Santos <julioc.9730@gmail.com>
2024-02-23 10:50:04 -0300

    Update instances.rb

--- features/support/instances.rb
+++ features/support/instances.rb
@@ -2,8 +2,8 @@ require_relative "capy"
 
 RSpec.configure do |config|
   ENV["HASH_USER"] = "bs://1a4806c03f9f7a42c350791dc3372ca0ed834dfa"
-  ENV["USER_BS"] = "nametest_CZgRBq"
-  ENV["HASH_KEY"] = "wgdJ6eNqMsyexkVUwhzp"
+  ENV["USER_BS"] = "nametest_xUmHeN"
+  ENV["HASH_KEY"] = "ujyrk3C1THqy6y5WXBdW"
   ENV["RETRIES"] = 0.to_s
 
   config.before(:each) do
