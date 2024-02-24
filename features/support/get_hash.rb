7926ca7d33c111982e4c547db1239403d4cd57ce
Julio Santos <julioc.9730@gmail.com>
2024-02-24 06:29:18 -0300

    Atualizar o get_hash.rb

--- features/support/get_hash.rb
+++ features/support/get_hash.rb
@@ -2,7 +2,7 @@ require 'httparty'
 
 base_url = "https://api-cloud.browserstack.com"
 
-response = HTTParty.post("#{base_url}/app-automate/espresso/v2/upload"
+response = HTTParty.post("#{base_url}/app-automate/espresso/v2/upload",
     basic_auth: { username: ENC["BS_USER"], password: ENV["BS_KEY"] }, 
     body: { file: File.new("/caminho/do/seu/arquivo.apk")
     headers: { "Content-Typ" : "multipart/form-data" }
