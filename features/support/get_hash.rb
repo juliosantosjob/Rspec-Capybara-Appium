feef6b8d861e956d5a856d68164095f4c6ec5c46
Julio Santos <julioc.9730@gmail.com>
2024-02-24 06:36:45 -0300

    Atualizar o get_hash.rb

--- features/support/get_hash.rb
+++ features/support/get_hash.rb
@@ -1,11 +1,13 @@
 require 'httparty'
 
 base_url = "https://api-cloud.browserstack.com"
+user_credential = ENV["BS_USER"]
+key_credencial = ENV["BS_KEY"]
 
 response = HTTParty.post("#{base_url}/app-live/upload",
     basic_auth: { 
-        username: ENV["BS_USER"], 
-        password: ENV["BS_KEY"] 
+        username: user_credential, 
+        password: key_credencial
     },
     body: { file: File.new("/caminho/do/seu/arquivo.apk"),
     headers: { "Content-Typ" : "multipart/form-data" }
