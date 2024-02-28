5a40e82d000de46d103566def09133de5029abdd
Julio Santos <julioc.9730@gmail.com>
2024-02-28 19:41:23 -0300

    Update instances.rb

--- features/support/instances.rb
+++ features/support/instances.rb
@@ -2,7 +2,6 @@ require_relative "capy"
 require_relative "../utils/helpers"
 
 RSpec.configure do |config|
-  include Helper
 
   config.before :each do
     @login ||= LoginScreen.new
@@ -12,10 +11,5 @@ RSpec.configure do |config|
     @drag_and_drop ||= DragAndDropScreen.new
     @long_press ||= LongPressScreen.new
     @carousel ||= CarouselScreen.new
-  end
-
-  config.before :each do
-    $login_elements = Helper.load_elements("features/locators/login.yml")
-    $carousel_elements = Helper.load_elements("features/locators/carousel.yml")
   end
 end
